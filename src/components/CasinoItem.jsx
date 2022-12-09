d02203eeea8e2e1dc0da878be7ff1200f381db52
Felipe Ferreira <fbhz85@gmail.com>
2022-12-09 17:49:38 -0300

    fix warning

--- src/components/CasinoItem.jsx
+++ src/components/CasinoItem.jsx
@@ -10,7 +10,7 @@ function CasinoItem({ casino: { website, content } }) {
 
             <div className="card shadow-2xl w-380 m-20">
                 <figure>
-                    <img src={content} />
+                    <img src={content} alt="casinoimg" />
                 </figure>
                 <div className="card-body">
                     <h2 className="card-title">New Casino for You</h2>
