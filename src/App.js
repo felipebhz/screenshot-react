118fce399c5eaa59c793e0c4b4b636b8a1f1062e
Felipe Ferreira <fbhz85@gmail.com>
2022-12-09 16:51:39 -0300

    add tailwind daisyui navbar footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { CasinoContextProvider } from './context/CasinoContext';
 import { useContext } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+
 import Home from './pages/Home';
 import About from './pages/About';
 
@@ -14,10 +17,16 @@ function App() {
   return (
     <CasinoContextProvider>
       <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+        <main className='container mx-auto px-3 pb-12'>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
         </Routes>
+        </main>
+        <Footer />
+        </div>
       </Router>
     </CasinoContextProvider>
   );
--- src/components/CasinoItem.jsx
+++ src/components/CasinoItem.jsx
@@ -3,8 +3,21 @@ import CasinoContext from '../context/CasinoContext';
 function CasinoItem({ casino: { website, content } }) {
     return (
         <>
-        <img src={content} width={800} />
-        <p>{website}</p>
+            {/* <img src={content} width={800} />
+            <p>{website}</p> */}
+
+            <div className="card lg:card-side bg-base-100 shadow-xl mb-20">
+                <figure>
+                    <img src={content} alt="Album" />
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title">Lorem ipsum dolor sit amet.</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur expedita mollitia esse quasi quibusdam fugiat nesciunt ipsam saepe sit deserunt modi officia quos nisi dignissimos atque, ullam necessitatibus illo. Illo.</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">Visit Casino Website</button>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
