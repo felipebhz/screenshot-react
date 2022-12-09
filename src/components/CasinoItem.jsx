9f961eb144539be553c011b8832d0a4b057dc936
Felipe Ferreira <fbhz85@gmail.com>
2022-12-09 17:18:28 -0300

    some UI increments

--- src/components/CasinoItem.jsx
+++ src/components/CasinoItem.jsx
@@ -13,8 +13,17 @@ function CasinoItem({ casino: { website, content } }) {
                     <img src={content} />
                 </figure>
                 <div class="card-body">
-                    <h2 class="card-title">DaisyUI Card</h2>
+                    <h2 class="card-title">New Casino for You</h2>
                     <p>Rerum reiciendis beatae tenetur excepturi aut pariatur est eos. Sit sit necessitatibus.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    <div>
+                        <button className="btn gap-2">
+                            Reviews
+                            <div className="badge">37</div>
+                        </button>
+
+
+                    </div>
                     <div className="rating">
                         <input type="radio" name="rating-4" className="mask mask-star-2 bg-yellow-500" />
                         <input type="radio" name="rating-4" className="mask mask-star-2 bg-yellow-500" checked />
@@ -22,7 +31,7 @@ function CasinoItem({ casino: { website, content } }) {
                         <input type="radio" name="rating-4" className="mask mask-star-2 bg-yellow-500" />
                         <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-200" />
                     </div>
-                    <button class="btn btn-accent">accent</button>
+                    <button class="btn btn-accent">Visit Casino Website</button>
                 </div>
             </div>
         </>
