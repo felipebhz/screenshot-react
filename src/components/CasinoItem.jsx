60702230b6a0ab0e37c779935a8e38893f449b7f
Felipe Ferreira <fbhz85@gmail.com>
2022-12-09 17:24:14 -0300

    removing warnings

--- src/components/CasinoItem.jsx
+++ src/components/CasinoItem.jsx
@@ -8,12 +8,12 @@ function CasinoItem({ casino: { website, content } }) {
   <div className="flex justify-center px-4 py-16 border-t border-base-300">Hello!</div>
 </div> */}
 
-            <div class="card shadow-2xl w-380 m-20">
+            <div className="card shadow-2xl w-380 m-20">
                 <figure>
                     <img src={content} />
                 </figure>
-                <div class="card-body">
-                    <h2 class="card-title">New Casino for You</h2>
+                <div className="card-body">
+                    <h2 className="card-title">New Casino for You</h2>
                     <p>Rerum reiciendis beatae tenetur excepturi aut pariatur est eos. Sit sit necessitatibus.</p>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
                     <div>
@@ -26,12 +26,12 @@ function CasinoItem({ casino: { website, content } }) {
                     </div>
                     <div className="rating">
                         <input type="radio" name="rating-4" className="mask mask-star-2 bg-yellow-500" />
-                        <input type="radio" name="rating-4" className="mask mask-star-2 bg-yellow-500" checked />
+                        <input type="radio" name="rating-4" className="mask mask-star-2 bg-yellow-500" checked readOnly/>
                         <input type="radio" name="rating-4" className="mask mask-star-2 bg-yellow-500" />
                         <input type="radio" name="rating-4" className="mask mask-star-2 bg-yellow-500" />
                         <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-200" />
                     </div>
-                    <button class="btn btn-accent">Visit Casino Website</button>
+                    <button className="btn btn-accent">Visit Casino Website</button>
                 </div>
             </div>
         </>
