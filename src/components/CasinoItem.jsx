d301928d126eda701faab1ca346d65a0f07c6866
Felipe Ferreira <fbhz85@gmail.com>
2022-12-09 16:34:22 -0300

    add daisy ui styles

--- src/components/CasinoItem.jsx
+++ src/components/CasinoItem.jsx
@@ -3,6 +3,7 @@ import CasinoContext from '../context/CasinoContext';
 function CasinoItem({ casino: { website, content } }) {
     return (
         <>
+        <img src={content} width={800} />
         <p>{website}</p>
         </>
     )
